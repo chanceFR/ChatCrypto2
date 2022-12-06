de4f659613e49e05dfa4f597febd91975e5c8942
romeo <lariviereromeo@gmail.com>
2022-12-06 14:38:31 +0100

    debut2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Main {
 
@@ -8,36 +11,36 @@ public class Main {
 
     }
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
 
-        ServerSocket serverSocket = null;
-        try {
-          serverSocket = new ServerSocket(4444);
-        } catch (IOException e) {
-            System.out.println("Could not listen on port 4444");
-            System.exit(-1);
+            ServerSocket serverSocket = null;
+            try {
+                serverSocket = new ServerSocket(4444);
+            } catch (IOException e) {
+                System.out.println("Could not listen on port 4444");
+                System.exit(-1);
+            }
+
+            Socket clientSocket = null;
+            try {
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.out.println("Accept failed on port 4444");
+                System.exit(-1);
+            }
+
+            try {
+                Socket echoSocket = new Socket(nom machine distante, numéro de port distant);
+                PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(newInputStreamReader(echoSocket.getInputStream()));
+            } catch (
+                    UnknownHostException e) {
+                System.out.println(" Destination unknown ");
+                System.exit(-1);
+            } catch (IOException e) {
+                System.out.println(" now to investigate this IO issue ");
+                System.exit(-1);
+            }
         }
-
-        Socket clientSocket = null;
-        try {
-            clientSocket = serverSocket.accept();
-        } catch (IOException e) {
-            System.out.println("Accept failed on port 4444");
-            System.exit(-1);
-        }
-    }
-    try{
-    	echoSocket = new Socket(nom machine distante, numéro de port distant) ;
-    	out = new PrintWriter(echoSocket.getOutputStream(),true) ;
-    	in = new BufferedReader(newInputStreamReader(echoSocket.getInputStream())) ;
-    	}
-    	catch(UnknownHostException e){
-    	System.out.println(« Destination unknown ») ;
-    	System.exit(-1) ;
-    	}
-    	catch(IOException e){
-    	System.out.println(« now to investigate this IO issue ») ;
-    	System.exit(-1) ;
-    	}
 }
