a19937bdf48006faca4de45eb468b1c35db46920
romeo <lariviereromeo@gmail.com>
2022-12-06 14:43:47 +0100

    debut3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -31,9 +32,9 @@ public class Main {
             }
 
             try {
-                Socket echoSocket = new Socket(nom machine distante, numéro de port distant);
+                Socket echoSocket = new Socket("test", 15);
                 PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(newInputStreamReader(echoSocket.getInputStream()));
+                BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
             } catch (
                     UnknownHostException e) {
                 System.out.println(" Destination unknown ");
